bdaef35f6b63572f207bafa09c6e29b21b8e5a9a
jantizio <emanuele0246@gmail.com>
2025-01-09 17:18:46 +0100

    add dockerfile

--- src/index.php
+++ src/index.php
@@ -1,47 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="common/example.css">
   <title>Accessibility examples</title>
-  
+
   <style>
-  body {
-font-size: 18px;
-background-color: #fffceb !important;
-padding: 1em 1em 1em 2em; 
-margin: 1em; 
-}
+    body {
+      font-size: 18px;
+      background-color: #fffceb !important;
+      padding: 1em 1em 1em 2em;
+      margin: 1em;
+    }
 
-li {
-padding: 0.25em;
-}
+    li {
+      padding: 0.25em;
+    }
 
-h1 {
-text-align: center;
-font-size: 36px;
-margin-top: 0.5em !important;
-}
+    h1 {
+      text-align: center;
+      font-size: 36px;
+      margin-top: 0.5em !important;
+    }
   </style>
-  
+
 </head>
+
 <body>
   <main>
     <h1 class="saharian-debug-h1">Accessibility examples</h1>
     <p>The good, the bad and the ugly of web accessibility.</p>
     <ul><?php
-    $examples = glob('*/index.html');
-    foreach ($examples as $example) {
-      $name = substr($example, 0, strpos($example, '/'));
-echo <<<example
+        $examples = glob('*/index.html');
+        foreach ($examples as $example) {
+          $name = substr($example, 0, strpos($example, '/'));
+          echo <<<example
 <li>
 <a href="$name/">$name</a>
 </li>\n
 example;
-    }
-?></ul>
+        }
+        ?></ul>
   </main>
 </body>
+
 </html>
