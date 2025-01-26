9e2926bd9d95a972573a91c7f659a7726c36962b
jantizio <emanuele0246@gmail.com>
2025-01-26 23:57:41 +0100

    feat: content on the saharian first page

--- src/index.php
+++ src/index.php
@@ -14,16 +14,58 @@
       background-color: #fffceb !important;
       padding: 1em 1em 1em 2em;
       margin: 1em;
-    }
-
-    li {
-      padding: 0.25em;
+      font-family: Arial, sans-serif;
+      line-height: 1.6;
     }
 
     h1 {
       text-align: center;
       font-size: 36px;
       margin-top: 0.5em !important;
+      color: #333;
+    }
+
+    h2 {
+      font-size: 28px;
+      margin-top: 1em;
+      color: #444;
+    }
+
+    p {
+      margin-bottom: 1em;
+      color: #555;
+    }
+
+    ul,
+    ol {
+      padding-left: 1.5em;
+    }
+
+    ul li {
+      margin: 0.5em 0;
+      padding: 0.25em;
+      list-style-type: disc;
+    }
+
+    ol li {
+      margin: 0.5em 0;
+    }
+
+    ul li a {
+      text-decoration: none;
+      color: #007BFF;
+      transition: color 0.3s;
+    }
+
+    ul li a:hover {
+      text-decoration: underline;
+      color: #0056b3;
+    }
+
+    ul li span {
+      font-size: 14px;
+      margin-left: 0.5em;
+      color: #666;
     }
   </style>
 
@@ -32,28 +74,47 @@
 <body>
   <main>
     <h1 class="saharian-debug-h1">SahARIAn</h1>
-    <p>The Sighted Architect's Helper for ARIA Notation. Download at <a href="saharian.zip">here</a>.</p>
+    <p>SahARIAn, the Sighted Architect's Helper for ARIA Notation, is a browser extension designed to simplify and enhance web accessibility. With SahARIAn, developers can easily identify, debug, and optimize ARIA attributes and accessible components within their web projects. This tool tries to show sighted developers how a screen reader would interpret the web page. </p>
+    <p>To use it click on the extension icon and select the level:</p>
+    <ul>
+      <li>Off - the extension is disabled, and the web page is rendered as usual</li>
+      <li>Min - is the base level, you will use this mode most of the time</li>
+      <li>Max - is a more advanced level, with focus on more advanced accessibility issues</li>
+    </ul>
+
+    <h2 class="saharian-debug-h2">How to Install:</h2>
+    <ol>
+      <li>Download the <a href="saharian.zip">compressed archive</a> of the extension.</li>
+      <li>Extract the downloaded folder and navigate to your browser's extensions page. Check out specific instruction for your browser of choice</li>
+      <ul>
+        <li><a href="https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension#installing" target="_blank">Firefox</a></li>
+        <li><a href="https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world#load-unpacked" target="_blank">Chrome</a></li>
+        <li><a href="https://learn.microsoft.com/en-us/microsoft-edge/extensions-chromium/getting-started/extension-sideloading" target="_blank">Edge</a></li>
+      </ul>
+    </ol>
 
     <h2 class="saharian-debug-h2">Accessibility examples</h2>
-    <p>The good, the bad and the ugly of web accessibility.</p>
-    <ul><?php
-        $ignored = ["useful TO-DO list"];
-        $examples = glob('*/index.html');
-        foreach ($examples as $example) {
-          $name = substr($example, 0, strpos($example, '/'));
+    <p>The good, the bad, and the ugly of web accessibility.</p>
+    <ul>
+      <?php
+      $ignored = ["useful TO-DO list"];
+      $examples = glob('*/index.html');
+      foreach ($examples as $example) {
+        $name = substr($example, 0, strpos($example, '/'));
 
-          if (in_array($name, $ignored)) {
-            continue;
-          }
+        if (in_array($name, $ignored)) {
+          continue;
+        }
 
-          echo
-          <<<example
+        echo
+        <<<example
           <li>
             <a href="$name/">$name</a>
           </li>\n
           example;
-        }
-        ?></ul>
+      }
+      ?>
+    </ul>
   </main>
 </body>
 
